68f1a0eecd85e1037d6ed25532d4c60582c7e12f
matiasscorsetti <47259070+matiasscorsetti@users.noreply.github.com>
2021-01-18 21:34:11 -0300

    Update parallel.py

--- parallel.py
+++ parallel.py
@@ -1,29 +1,26 @@
-'''
-
-Function to parallelize the application of processes on a list of objects
-
-Example:
-
-def my_function(constant, optional=1):
-
-    a = constant ** (2 * optional)
-
-    return a
-
-my_object = [4, 4, 4, 4, 8, 8, 8, 8]
-kwargs={'optional': 100000000}
-cpus=4
-
-results = run_in_parallel(my_function, my_object, kwargs=kwargs, cpus=cpus)
-list_results = [i for i in results]
-
-'''
-
 from functools import partial
 from multiprocessing import get_context, cpu_count
 
 
 def run_in_parallel(function, object_list, kwargs=None, cpus=-1, ordered=False):
+    '''
+    Function to parallelize the application of processes on a list of objects
+
+    Example:
+
+        def my_function(constant, optional=1):
+
+            a = constant ** (2 * optional)
+
+            return a
+
+        my_object = [4, 4, 4, 4, 8, 8, 8, 8]
+        kwargs={'optional': 100000000}
+        cpus=4
+
+        results = run_in_parallel(my_function, my_object, kwargs=kwargs, cpus=cpus)
+        list_results = [i for i in results]
+    '''
 
     if cpus==-1:
         cpus = cpu_count()
